3379fd60850e1435b04489a4fe724f2d868c9132
ttrampp <ttrampp@msn.com>
2025-07-26 16:36:34 -0600

    next.js chapters 7-9 complete

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,6 +11,7 @@ import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
